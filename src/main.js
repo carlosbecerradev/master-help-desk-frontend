d175f87bdd866ae9f8308129dadba4afb2a3f44a
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-05-31 17:19:37 -0500

    :lipstick: Add style file.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './styles.css'
 
 Vue.config.productionTip = false
 
