b78190e09dd6f02769e879dbee0c186a6901f16a
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-05 03:33:25 -0500

    :sparkles: Add UserDropdown component.

---
+++ src/components/auth/UserDropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ml-auto">
+    <b-nav pills>
+      <b-nav-item-dropdown
+        id="my-nav-dropdown"
+        toggle-class="nav-link-custom bg-white text-dark"
+        right
+      >
+        <template #button-content>
+          <span class="mr-2">{{ JWTClaims.jwt_subject }}</span>
+          <b-icon icon="person-circle" aria-hidden="true"></b-icon>
+        </template>
+        <b-dropdown-item @click="logout">Cerrar Sesión</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["JWTClaims"]),
+  },
+  methods: {
+    ...mapActions(["logout"]),
+  },
+};
+</script>
