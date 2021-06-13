6fec7256acf72a38a545e7788e0f23a86300017f
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-13 14:28:09 -0500

    :hammer: update Date time format.

--- src/utils/format/DateTimeFormatUtil.js
+++ src/utils/format/DateTimeFormatUtil.js
@@ -1,4 +1,8 @@
 export default function formatToDMYHM(dateISO) {
     const d = new Date(dateISO);
-    return `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+    const hour = d.getHours();
+    const formatHour = hour >= 0 && hour <= 9 ? `0${hour}` : hour;
+    const minutes = d.getMinutes();
+    const formatMinutes = minutes >= 0 && minutes <= 9 ? `0${minutes}` : minutes;
+    return `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} ${formatHour}:${formatMinutes}`;
 }
