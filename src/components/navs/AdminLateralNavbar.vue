a88b2f29d9d27372cc8c4ba7cd74a2abdb778ce0
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-22 17:06:00 -0500

    :hammer: Add links to request type page.

--- src/components/navs/AdminLateralNavbar.vue
+++ src/components/navs/AdminLateralNavbar.vue
@@ -69,6 +69,19 @@
             </b-list-group-item>
           </b-list-group>
         </details>
+
+        <details>
+          <summary class="lateral-navbar-nav-item">Tipo de Solicitudes</summary>
+          <b-list-group>
+            <b-list-group-item>
+              <router-link
+                to="/tipo-de-solicitudes/listar"
+                class="lateral-navbar-nav-item-link d-block"
+                >Listar</router-link
+              >
+            </b-list-group-item>
+          </b-list-group>
+        </details>
       </div>
     </div>
   </div>
--- src/components/navs/AnalistaLateralNavbar.vue
+++ src/components/navs/AnalistaLateralNavbar.vue
@@ -15,7 +15,19 @@
           <b-list-group>
             <b-list-group-item>
               <router-link
-                to="/solicitudes-recibidas"
+                to="/analista/solicitudes"
+                class="lateral-navbar-nav-item-link d-block"
+                >Listar</router-link
+              >
+            </b-list-group-item>
+          </b-list-group>
+        </details>
+        <details>
+          <summary class="lateral-navbar-nav-item">Tipo de Solicitudes</summary>
+          <b-list-group>
+            <b-list-group-item>
+              <router-link
+                to="/tipo-de-solicitudes/listar"
                 class="lateral-navbar-nav-item-link d-block"
                 >Listar</router-link
               >
